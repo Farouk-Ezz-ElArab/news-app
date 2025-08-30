e7e25b82647f873a8bedff55a6d574666727c3a3
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-08-30 18:33:07 +0300

    retrofit package for api

--- lib/ui/home/category_details/category_details.dart
+++ lib/ui/home/category_details/category_details.dart
@@ -1,12 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/ui/home/category_details/sources/source_tab_widget.dart';
+import 'package:news_app/api/api_constants.dart';
+import 'package:news_app/api/retrofit_services.dart';
 
-import '../../../api/api_manager.dart';
 import '../../../l10n/app_localizations.dart';
-import '../../../model/SourceResponse.dart';
 import '../../../model/category.dart';
 import '../../../utils/app_colors.dart';
-
 class CategoryDetails extends StatefulWidget {
   Category category;
 
@@ -19,8 +18,9 @@ class CategoryDetails extends StatefulWidget {
 class _CategoryDetailsState extends State<CategoryDetails> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<SourceResponse?>(
-      future: ApiManager.getSources(widget.category.id),
+    return FutureBuilder(
+      future: RetrofitServices(Dio()).getSources(
+          ApiConstants.apiKey, widget.category.id),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
@@ -33,52 +33,59 @@ class _CategoryDetailsState extends State<CategoryDetails> {
               children: [
                 Text(
                   AppLocalizations.of(context)!.something_went_wrong,
-                  style: Theme.of(context).textTheme.labelMedium,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .labelMedium,
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: AppColors.greyColor,
                   ),
                   onPressed: () {
-                    ApiManager.getSources(widget.category.id);
+                    RetrofitServices(Dio()).getSources(
+                        ApiConstants.apiKey, widget.category.id);
                     setState(() {});
                   },
                   child: Text(
                     AppLocalizations.of(context)!.try_again,
-                    style: Theme.of(context).textTheme.labelMedium,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .labelMedium,
                   ),
                 ),
               ],
             ),
           );
         }
-        if (snapshot.data?.status != 'ok') {
-          return Center(
-            child: Column(
-              children: [
-                Text(
-                  snapshot.data!.message!,
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.greyColor,
-                  ),
-                  onPressed: () {
-                    ApiManager.getSources(widget.category.id);
-                    setState(() {});
-                  },
-                  child: Text(
-                    AppLocalizations.of(context)!.try_again,
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ),
-              ],
-            ),
-          );
-        }
-        var sourcesList = snapshot.data?.sources ?? [];
-        return SourceTabWidget(sourcesList: sourcesList);
+        // if (snapshot.data?.status != 'ok') {
+        //   return Center(
+        //     child: Column(
+        //       children: [
+        //         Text(
+        //           snapshot.data!.message!,
+        //           style: Theme.of(context).textTheme.labelMedium,
+        //         ),
+        //         ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             backgroundColor: AppColors.greyColor,
+        //           ),
+        //           onPressed: () {
+        //             ApiManager.getSources(widget.category.id);
+        //             setState(() {});
+        //           },
+        //           child: Text(
+        //             AppLocalizations.of(context)!.try_again,
+        //             style: Theme.of(context).textTheme.labelMedium,
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   );
+        // }
+        // var sourcesList = snapshot.data?.sources ?? [];
+        // return SourceTabWidget(sourcesList: sourcesList);
       },
     );
   }
